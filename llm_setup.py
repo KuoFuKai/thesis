e6c9130684287b820d8bdf50d5727958a62fbd4a
KuoFuKai <100160765+KuoFuKai@users.noreply.github.com>
2024-02-01 01:32:00 +0800

    init-1

---
+++ llm_setup.py
@@ -0,0 +1,41 @@
+import torch
+from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+
+
+def llm_setup(model_id):
+    # 配置BitsAndBytes的設定
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_use_double_quant=True,
+    )
+
+    # 加載模型和分詞器
+    model_4bit = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        device_map="auto",
+        quantization_config=quantization_config,
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+    # 創建pipeline
+    text_generation_pipeline = pipeline(
+        "text-generation",
+        model=model_4bit,
+        tokenizer=tokenizer,
+        use_cache=True,
+        device_map="auto",
+        max_length=1500,
+        do_sample=True,
+        top_k=5,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.eos_token_id,
+    )
+
+    llm = HuggingFacePipeline(pipeline=text_generation_pipeline, )
+
+    return llm
---
+++ yolo_inference.py
@@ -0,0 +1,66 @@
+# yolo_inference.py
+from ultralytics import YOLO  # 引入 YOLO 模型
+import cv2  # 引入 OpenCV 庫
+import math  # 引入數學庫用于計算
+from queue import Queue
+
+detected_queue = Queue()
+conf_threshold = 0.8
+
+
+def yolo_inference(source):
+    # 确定视频来源是摄像头还是视频文件
+    if source == "webcam":
+        cap = cv2.VideoCapture(0)
+    else:
+        cap = cv2.VideoCapture(source)
+
+    cap.set(3, 640)  # 设置宽度
+    cap.set(4, 480)  # 设置高度
+
+    # 加载 YOLO 模型
+    model = YOLO("best.pt")
+    classNames = ["tiger", "tiger-cub", "white-tiger"]
+
+    max_confidence = 0
+
+    paused = False  # 暂停状态
+
+    while True:
+        if not paused:
+            success, img = cap.read()
+            if not success:
+                break  # 视频结束或摄像头关闭时退出循环
+
+            results = model(img, stream=True)
+
+            for r in results:
+                boxes = r.boxes
+                for box in boxes:
+                    cls = int(box.cls[0])
+                    confidence = math.ceil(box.conf[0] * 100) / 100
+                    if confidence >= conf_threshold:
+                        x1, y1, x2, y2 = map(int, box.xyxy[0])
+                        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                        cv2.putText(img, f"{classNames[cls]} {confidence:.2f}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                    (255, 0, 0), 2)
+                        if confidence > max_confidence:
+                            max_confidence = confidence
+                            detected_queue.put(classNames[cls])
+
+        cv2.imshow('YOLO Inference', img)
+
+        # 按键处理
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):  # 按 'q' 退出
+            break
+        elif key == ord(' '):  # 按空格键暂停/继续
+            paused = not paused
+
+    # 釋放資源幷關閉窗口
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    yolo_inference("test.mp4")
