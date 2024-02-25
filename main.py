4e1fee56a9931dc645a3c2fcc5c029de3f57103a
KuoFuKai <100160765+KuoFuKai@users.noreply.github.com>
2024-02-26 00:10:29 +0800

    init-18

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import argparse
 import threading
 from ultralytics import YOLO  # 引入 YOLO 模型
 from llm_setup import llm_setup
 from llm_util import interact
 from yolo_util import inference
 from tts_util import say
+
+# 設定命令行參數
+parser = argparse.ArgumentParser(description="啟動物件辨識和語言互動系統")
+parser.add_argument("--source", type=str, default="webcam", help="指定影像來源，可以是 'CSI'、'webcam' 或影片的路徑")
+args = parser.parse_args()
 
 if __name__ == '__main__':
     say("程式初始化請稍後")
@@ -14,6 +20,6 @@ if __name__ == '__main__':
     model = YOLO("best.pt")
     say("載入物件辨識模型成功")
     # 執行物件辨識
-    threading.Thread(target=inference, args=("webcam", model, llm,)).start()
+    threading.Thread(target=inference, args=(args.source, model, llm,)).start()
     # 進行QA環節
     threading.Thread(target=interact, args=(llm,)).start()
