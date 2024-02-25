f5023e72c2d2a6cbb10c1b544593222fe0ddcb8b
KuoFuKai <100160765+KuoFuKai@users.noreply.github.com>
2024-02-26 02:34:20 +0800

    init-19

--- csi_camera.py
+++ csi_camera.py
@@ -1,35 +1,59 @@
+import sys
 import cv2
-print(cv2.getBuildInformation())
 
-# Reading BGRx frames into opencv:
-cap = cv2.VideoCapture(
-    "nvarguscamerasrc ! video/x-raw(memory:NVMM),width=3264, height=2464, framerate=21/1, format=NV12 ! nvvidconv ! "
-    "video/x-raw,format=BGRx ! appsink drop=1",
-    cv2.CAP_GSTREAMER)
 
-# Or reading BGR frames as expected by most opencv algorithms: cap = cv2.VideoCapture("nvarguscamerasrc !
-# video/x-raw(memory:NVMM),width=3264, height=2464, framerate=21/1, format=NV12 ! nvvidconv ! video/x-raw,format=BGRx
-# ! videoconvert ! video/x-raw,format=BGR ! appsink drop=1", cv2.CAP_GSTREAMER)
+def gstreamer_pipeline(
+        sensor_id=0,
+        capture_width=1280,
+        capture_height=720,
+        display_width=960,
+        display_height=540,
+        framerate=60,
+        flip_method=0,
+):
+    return (
+            "nvarguscamerasrc sensor-id=%d !"
+            "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink"
+            % (
+                sensor_id,
+                capture_width,
+                capture_height,
+                framerate,
+                flip_method,
+                display_width,
+                display_height,
+            )
+    )
 
-if not cap.isOpened():
-    print("failed to open video capture")
-    exit(-1)
 
-cv2.namedWindow("CamPreview", cv2.WINDOW_AUTOSIZE)
+def main():
+    print(gstreamer_pipeline(flip_method=0))
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    print(True)
+    frame = [None] * 1
+    if cap.isOpened():
+        try:
+            while True:
+                ret, frame[0] = cap.read()
+                if ret:
+                    # cv2.imshow("video", frame)
+                    print(True)
+                else:
+                    print("Not read")
 
-frames = 0
-# Run for 10s @21fps
-while frames < 210:
-    ret_val, img = cap.read()
-    if not ret_val:
-        print("failed to read from video capture")
-        break
-    frames = frames + 1
+                keyCode = cv2.waitKey(10) & 0xFF
+                if keyCode == 27 or keyCode == ord('q'):
+                    break
+        except KeyboardInterrupt:
+            cap.release()
+            cv2.destroyAllWindows()
+            print("Bye")
+            sys.exit()
 
-    cv2.imshow('CamPreview', img)
 
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-cv2.destroyAllWindows()
-cap.release()
+if __name__ == "__main__":
+    main()
