6ac290465697688eb73afa0a48dbba9c56b77278
KuoFuKai <100160765+KuoFuKai@users.noreply.github.com>
2024-02-26 00:16:13 +0800

    init-19

--- csi_camera.py
+++ csi_camera.py
@@ -2,10 +2,10 @@ import cv2
 
 
 def gstreamer(
-        capture_width=640,
-        capture_height=480,
-        display_width=640,
-        display_height=480,
+        capture_width=1280,
+        capture_height=720,
+        display_width=1280,
+        display_height=720,
         framerate=60,
         flip_method=0,
 ):
